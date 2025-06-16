68c28bae5e20892e5661105244be95e5c95c55c9
Nishant Yadav <nynishuyadav85@gmail.com>
2025-06-16 12:24:28 +0530

    CountDigit

--- WarmUp/CountDigit.js
+++ WarmUp/CountDigit.js
@@ -1,1 +1,16 @@
-console.log('countDigit')
+function countDigit(num) {
+    let count = 0;
+    if (num == 0) return 1
+    num = Math.abs(num)
+    while (num > 0) {
+        num = Math.floor(num / 10);
+        count++;
+    }
+    return count;
+}
+
+let num = -123444;
+
+let value = countDigit(num)
+
+console.log(value)
